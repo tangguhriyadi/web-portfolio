66d5770a3098f1580e9bcc115927bb210f91295e
tangguhriyadi <mtangguh97@gmail.com>
2024-03-09 20:00:44 +0700

    feat(nav): mobile-nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { AlignJustify } from "lucide-react";
+import Logo from "./Logo";
+import Nav from "./Nav";
+import Socials from "./Socials";
 
-const MobileNav = () => {
+const MobileNav: React.FC = () => {
   return (
-    <div>MobileNav</div>
-  )
-}
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className="cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex flex-col items-center justify-between h-full py-8">
+          <div className="flex flex-col items-center gap-y-32">
+            <Logo />
+            <Nav
+              containerStyles="flex flex-col items-center gap-y-6"
+              linkStyles="text-2xl"
+            />
+          </div>
+          <Socials containerStyles="flex gap-x-4" iconStyles="text-2xl" />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
