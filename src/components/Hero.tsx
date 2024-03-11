530f5011efaeb240f0245922f78257bd404a177a
tangguhriyadi <mtangguh97@gmail.com>
2024-03-12 05:14:36 +0700

    fix(responsive): responsive ui fixing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ import Socials from "./Socials";
 
 const Hero: React.FC = () => {
   return (
-    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
+    <section className="py-12 xl:py-24 h-[95vh] lg:h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
       <div className="container mx-auto">
         <div className="flex justify-between gap-x-8">
           {/* text */}
@@ -74,7 +74,7 @@ const Hero: React.FC = () => {
             />
           </div>
         </div>
-        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-0 animate-bounce">
           <RiArrowDownSLine className="text-3xl text-primary" />
         </div>
       </div>
