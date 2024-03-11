6cd4e0c30b08ebdf58d32420bfb5e5a120c2d147
tangguhriyadi <mtangguh97@gmail.com>
2024-03-11 12:57:49 +0700

    feat(cta): cta ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
-import Reviews from "../components/Reviews";
+import Reviews from "@/components/Reviews";
+import Cta from "@/components/Cta";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Services />
       <Work />
       <Reviews />
+      <Cta />
     </main>
   );
 }
