2a8df48c4321d6be3a1cd1c3f49291dcdce5e273
tangguhriyadi <mtangguh97@gmail.com>
2025-02-25 12:07:51 +0700

    enhance: update CV

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -37,7 +37,7 @@ const Hero: React.FC = () => {
                   Contact Me <Send size={18} />
                 </Button>
               </Link>
-              <Link href="/CV_05192024.pdf" target="_blank">
+              <Link href="/tangguh-resume.pdf" target="_blank">
                 <Button variant="secondary" className="gap-x-2">
                   Download Resume <Download size={18} />
                 </Button>
